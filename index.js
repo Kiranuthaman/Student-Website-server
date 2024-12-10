b297a5cf366fe8eb4c2ec5c7b6a20b890b7039de
KIRAN UTHAMAN <kiranuthaman85@gmail.com>
2024-12-10 18:41:55 +0530

    Student server

---
+++ index.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server')
+const student = jsonServer.create()
+const middleware = jsonServer.defaults()
+
+const router = jsonServer.router('db.json')
+student.use(middleware)
+student.use(router)
+
+const PORT = 400 || process.env.PORT
+
+
+student.listen(PORT,()=>{
+    console.log(`server is running sucessfully at port number ${PORT}`);
+    
+})
