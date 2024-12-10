e390cbf2cdfa00bb4e8bc7187da3a20544758d16
KIRAN UTHAMAN <kiranuthaman85@gmail.com>
2024-12-10 19:03:31 +0530

    student Server

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const router = jsonServer.router('db.json')
 student.use(middleware)
 student.use(router)
 
-const PORT = 400 || process.env.PORT
+const PORT = 4001 || process.env.PORT
 
 
 student.listen(PORT,()=>{
